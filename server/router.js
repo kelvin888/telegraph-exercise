c70706d28b64cf42f40f752073a2154deec1501e
“kelvin888” <kelvinorhungul@azeelodigital.com>
2021-10-23 16:04:12 +0100

    Telegraph assignment implementation

--- server/router.js
+++ server/router.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const db = require("../db.json");
 
-router.get('/', (req, res) => {
-	res.render("home", {
-		meta: req.app.get('meta')
-	});
+router.get("/", (req, res) => {
+  res.render("home", {
+    meta: req.app.get("meta"),
+    comments: db.comments,
+  });
 });
 
 module.exports = router;
